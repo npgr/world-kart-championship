f21297fb005cae443ad2a8e79f85e8f36d63ceac
Nuno <ngocalves@gmail.com>
2020-12-04 19:04:16 +0100

    DriverRace Model: add milliseconds property

--- src/models/DriverRace.ts
+++ src/models/DriverRace.ts
@@ -3,6 +3,7 @@ export class DriverRace {
     public driverId: string,
     public raceId: string,
     public time: string,
+    public milliseconds: number,
     public position: number,
     public points: number,
     public accumulatedPoints: number
