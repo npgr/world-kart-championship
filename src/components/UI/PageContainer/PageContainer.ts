87a2962c7e7d5d57be953362a1728ef47b151c92
Nuno <ngocalves@gmail.com>
2020-12-07 18:51:38 +0100

    litle changle on PageContainer style

--- src/components/UI/PageContainer/PageContainer.ts
+++ src/components/UI/PageContainer/PageContainer.ts
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 import Container from '@material-ui/core/Container';
 
 export const PageContainer = styled(Container)`
-  padding: 1rem 0;
+  padding: 1rem 2rem;
 `;
