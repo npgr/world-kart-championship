ad4eb86d3aa39a2e6b5cdc88eafec7b0634a9a73
Nuno <ngocalves@gmail.com>
2020-12-08 18:01:17 +0100

    remove reportWebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import store from './store';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import MuiThemeProvider from '@material-ui/styles/ThemeProvider';
 import { ThemeProvider } from 'styled-components';
@@ -28,4 +27,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
